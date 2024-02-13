93dad5f1bc0e8d801dce634ff37c69a8089e9221
andreaskomplex <andreas.lehmann@komplexitaeter.de>
2024-02-13 14:01:22 +0100

    - audio input

--- script.js
+++ script.js
@@ -10,11 +10,41 @@ document.getElementById('cameraInput').onchange = function(event) {
     }
 };
 
+document.getElementById('recordAudioButton').onclick = function() {
+    document.getElementById('audioInput').click();
+};
+
+document.getElementById('audioInput').onchange = function(event) {
+    const file = event.target.files[0];
+    if (file) {
+        // Hier können Sie die Audiodatei per API an Ihren Server senden.
+        uploadAudio(file);
+    }
+};
+
+
 function uploadPhoto(file) {
     const formData = new FormData();
     formData.append('photo', file);
 
     fetch('api/upload_image.php', {
+        method: 'POST',
+        body: formData
+    })
+        .then(response => response.json())
+        .then(data => {
+            console.log('Erfolg:', data);
+        })
+        .catch((error) => {
+            console.error('Fehler:', error);
+        });
+}
+
+function uploadAudio(file) {
+    const formData = new FormData();
+    formData.append('audio', file);
+
+    fetch('api/upload_audio.php', {
         method: 'POST',
         body: formData
     })
